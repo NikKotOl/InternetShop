8a2da2efbe2d38812d0dced8b000782d7d5f130e
Kotenkov Nikita <nikitakotenkov095@gmail.com>
2026-07-15 17:50:33 +0400

    added first schema

---
+++ app/schemas/category.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, Field, ConfigDict
+from datetime import datetime
+
+
+class CategoryCreate(BaseModel):
+    name: str = Field(
+        min_length=1,
+        max_length=100,
+    )
+
+
+
+class CategoryResponse(CategoryCreate):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    created_at: datetime
+    updated_at: datetime
