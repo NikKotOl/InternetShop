e6fc1c29d59e2e8440c39e56358c890a502418c4
Kotenkov Nikita <nikitakotenkov095@gmail.com>
2026-07-22 22:45:09 +0400

    .

--- app/services/productService.py
+++ app/services/productService.py
@@ -1,35 +1,36 @@
 from typing import Sequence
 
 from app.core.exceptions import CategoryNotFoundError, ProductNotFoundError
-from app.models.categoryModel import CategoryModel
 from app.models.productModel import ProductModel
 from app.repositories.categoryRepository import CategoryRepository
 from app.repositories.productRepository import ProductRepository
-from app.core.exceptions import ProductNotFoundError
 
 
 class ProductService:
 
-    def __init__(self, productRepository: ProductRepository, categoryRepository: CategoryRepository):
+    def __init__(
+        self,
+        productRepository: ProductRepository,
+        categoryRepository: CategoryRepository,
+    ):
         self.productRepository = productRepository
         self.categoryRepository = categoryRepository
-    
 
     async def add_product(self, name: str, category_id: int) -> ProductModel:
         category = await self.categoryRepository.get_category_by_id(category_id)
         if category is None:
             raise CategoryNotFoundError(category_id)
         return await self.productRepository.add_product(name, category_id)
-    
 
     async def delete_product(self, id: int) -> ProductModel:
         product = await self.productRepository.get_product_by_id(id)
         if product is None:
             raise ProductNotFoundError(id)
         return await self.productRepository.delete_product(product)
-    
 
-    async def get_products_by_category_id(self, category_id: int) -> Sequence[ProductModel]:
+    async def get_products_by_category_id(
+        self, category_id: int
+    ) -> Sequence[ProductModel]:
         category = await self.categoryRepository.get_category_by_id(category_id)
         if category is None:
             raise CategoryNotFoundError(category_id)
