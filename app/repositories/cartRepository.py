fdcdd2d7cb579d76fddd15951465496baa4de711
Kotenkov Nikita <nikitakotenkov095@gmail.com>
2026-08-02 18:41:04 +0400

    Update cartRepository.py

--- app/repositories/cartRepository.py
+++ app/repositories/cartRepository.py
@@ -1,9 +1,10 @@
 from typing import Optional, Sequence
 
-from sqlalchemy import select
+from sqlalchemy import Row, select
 
 from app.models.cartItemModel import CartModel
 from app.db.database import AsyncSession
+from app.models.productModel import ProductModel
 
 
 class CartRepository:
@@ -45,4 +46,15 @@ class CartRepository:
             CartModel.user_id == user_id, CartModel.product_id == product_id
         )
         result = await self.session.execute(stmt)
-        return result.scalar()
+        return result.scalar_one_or_none()
+
+    async def get_cart_items_with_products_by_user_id(
+        self, user_id: int
+    ) -> Sequence[Row[tuple[CartModel, ProductModel]]]:
+        stmt = (
+            select(CartModel, ProductModel)
+            .join(ProductModel, CartModel.product_id == ProductModel.id)
+            .where(CartModel.user_id == user_id)
+        )
+        result = await self.session.execute(stmt)
+        return result.all()
