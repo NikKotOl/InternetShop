ca050fb3750e41758e0590b5aa3b01ffe8dd5cf4
Kotenkov Nikita <nikitakotenkov095@gmail.com>
2026-08-02 17:42:48 +0400

    Create cartRepository.py

---
+++ app/repositories/cartRepository.py
@@ -0,0 +1,48 @@
+from typing import Optional, Sequence
+
+from sqlalchemy import select
+
+from app.models.cartItemModel import CartModel
+from app.db.database import AsyncSession
+
+
+class CartRepository:
+    session: AsyncSession
+
+    def __init__(self, session):
+        self.session = session
+
+    async def add_cart(self, user_id: int, product_id: int, quantity: int) -> CartModel:
+        new_cart = CartModel(user_id=user_id, product_id=product_id, quantity=quantity)
+        self.session.add(new_cart)
+        await self.session.commit()
+        await self.session.refresh(new_cart)
+        return new_cart
+
+    async def quantity_update(self, cart: CartModel, quantity: int) -> CartModel:
+        cart.quantity = quantity
+        await self.session.commit()
+        await self.session.refresh(cart)
+        return cart
+
+    async def delete_cart_item(self, id: int) -> Optional[CartModel]:
+        cart = await self.session.get(CartModel, id)
+        if not cart:
+            return None
+        await self.session.delete(cart)
+        await self.session.commit()
+        return cart
+
+    async def get_carts_by_user_id(self, user_id: int) -> Sequence[CartModel]:
+        stmt = select(CartModel).where(CartModel.user_id == user_id)
+        carts = await self.session.execute(stmt)
+        return carts.scalars().all()
+
+    async def get_cart_item_by_user_and_product(
+        self, user_id: int, product_id: int
+    ) -> Optional[CartModel]:
+        stmt = select(CartModel).where(
+            CartModel.user_id == user_id, CartModel.product_id == product_id
+        )
+        result = await self.session.execute(stmt)
+        return result.scalar()
