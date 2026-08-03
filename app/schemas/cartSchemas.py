ca2240110ff626cfd6040f1982afdedbd40db542
Kotenkov Nikita <nikitakotenkov095@gmail.com>
2026-08-03 10:04:37 +0400

    Update cartSchemas.py

--- app/schemas/cartSchemas.py
+++ app/schemas/cartSchemas.py
@@ -1,6 +1,6 @@
 from decimal import Decimal
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, ConfigDict, Field
 
 
 class CartItemResponseSchema(BaseModel):
@@ -10,3 +10,16 @@ class CartItemResponseSchema(BaseModel):
     price: Decimal = Field(gt=0)
     quantity: int = Field(gt=0)
     subtotal: Decimal = Field(gt=0)
+
+
+class CartAddSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    product_id: int
+    quantity: int = Field(gt=0)
+
+
+class CartAddResponseSchema(CartAddSchema):
+    model_config = ConfigDict(from_attributes=True)
+    id: int
+    user_id: int
