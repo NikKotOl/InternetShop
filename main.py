4df62ba278b91b84b0bef20607499c210d19919b
Kotenkov Nikita <nikitakotenkov095@gmail.com>
2026-08-03 17:55:58 +0400

    Update main.py

--- main.py
+++ main.py
@@ -65,4 +65,4 @@ application.add_exception_handler(
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
+    uvicorn.run("main:application", host="127.0.0.1", port=8080, reload=True)
