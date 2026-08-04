d86a91d05df0e45ed03705e01aadabd1c2939c62
Kotenkov Nikita <nikitakotenkov095@gmail.com>
2026-08-04 13:23:11 +0400

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,5 +2,6 @@ from app.models.categoryModel import CategoryModel
 from app.models.productModel import ProductModel
 from app.models.userModel import UserModel
 from app.models.cartItemModel import CartModel
+from app.models.orderModel import OrderModel
 
-__all__ = ("CategoryModel", "ProductModel", "UserModel", "CartModel")
+__all__ = ("CategoryModel", "ProductModel", "UserModel", "CartModel", "OrderModel")
