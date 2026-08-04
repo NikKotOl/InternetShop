93dde52097ba9563111fa49896cbf94421c3aebd
Kotenkov Nikita <nikitakotenkov095@gmail.com>
2026-08-04 14:26:55 +0400

    Update orderModel.py

--- app/models/orderModel.py
+++ app/models/orderModel.py
@@ -19,7 +19,7 @@ class OrderItemModel(Base):
     __tablename__ = "order_items"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    order_id: Mapped[int] = mapped_column(ForeignKey("orders.id"), ondelete="CASCADE")
+    order_id: Mapped[int] = mapped_column(ForeignKey("orders.id", ondelete="CASCADE"))
     product_id: Mapped[int] = mapped_column(ForeignKey("products.id"))
     product_name: Mapped[str] = mapped_column(String(100), nullable=False)
     price_at_purchase: Mapped[Decimal] = mapped_column(Numeric(10, 2), nullable=False)
