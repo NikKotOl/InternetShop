0e5b9f4d0de817b3f23951da48c86574f49f99d7
Kotenkov Nikita <nikitakotenkov095@gmail.com>
2026-08-06 10:31:02 +0400

    Update orderService.py

--- app/services/orderService.py
+++ app/services/orderService.py
@@ -16,23 +16,27 @@ class OrderService:
         self.order_repository = order_repository
         self.cart_repository = cart_repository
 
-    async def create_order(self, user_id: int) -> OrderModel:
+    async def create_order(
+        self, user_id: int
+    ) -> tuple[OrderModel, list[OrderItemModel]]:
         cart_items = await self.cart_repository.get_cart_items_with_products_by_user_id(
             user_id
         )
         if not cart_items:
             raise EmptyCartError(user_id)
+        items = []
         order = await self.order_repository.add_order(user_id)
         for item in cart_items:
-            await self.order_repository.add_order_item(
+            order_item = await self.order_repository.add_order_item(
                 order_id=order.id,
                 product_id=item[0].product_id,
                 product_name=item[1].name,
                 price_at_purchase=item[1].price,
                 quantity=item[0].quantity,
             )
+            items.append(order_item)
             await self.cart_repository.delete_cart_item(item[0].id)
-        return order
+        return order, items
 
     async def get_order_by_id(
         self, order_id: int, user_id: int
